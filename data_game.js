706d2c88ecda57e631165201590dd125bbfa36aa
Ubaidillah30 <Maulidulula03@gmail.com>
2025-10-21 21:08:29 +0700

    Add files via upload

---
+++ data_game.js
@@ -0,0 +1,68 @@
+const dataProduk = {
+    'MLBB': {
+        id: 'MLBB',
+        nama: 'Mobile Legends Bang Bang',
+        gambar: 'gambar/mlgg.png',
+        diamond_image: 'gambar/3.png',
+        deskripsi: 'Top up Diamond ML hanya dalam hitungan detik! Cukup masukan User ID & Server MLBB Anda, Pilih jumlah Diamond yang Anda inginkan, Selesaikan pembayaran Dan Diamond akan langsung masuk ke akun Mobile Legends Anda.',
+        diamond: [
+            { total: '12 diamond (11 + 1 bonus)', jumlah: 12, harga: 3500 },
+            { total: '59 Diamonds (53 + 6 Bonus)', jumlah: 59, harga: 17000 },
+            { total: '170 Diamonds (154 + 16 Bonus)', jumlah: 170, harga: 48000 },
+            { total: '568 Diamonds (503 + 65 Bonus)', jumlah: 568, harga: 159000 },
+            { total: '1045 Diamonds (933 + 112 Bonus)', jumlah: 1045, harga: 299000 },
+            { total: '568 Diamonds (503 + 65 Bonus)', jumlah: 1500, harga: 435000 },
+            { total: '568 Diamonds (503 + 65 Bonus)', jumlah: 2010, harga: 525000 },
+            { total: '568 Diamonds (503 + 65 Bonus)', jumlah: 2625, harga: 724000 },
+            { total: '568 Diamonds (503 + 65 Bonus)', jumlah: 4020, harga: 1050000 },
+            { total: '568 Diamonds (503 + 65 Bonus)', jumlah: 6840, harga: 1785000 },
+        ]
+    },
+    'VALO': {
+        id: 'VALO',
+        nama: 'Valorant Points',
+        gambar: 'gambar/valo.png',
+        diamond_image: 'gambar/1.png',
+        deskripsi: 'Top up Valorant Points (VP) Anda di sini. Masukkan Riot ID, pilih jumlah VP, dan segera miliki skin impian Anda!',
+        diamond: [
+            { total: '125 Valorant Points', jumlah: 125, harga: 15000 },
+            { total: '420 Valorant Points', jumlah: 420, harga: 48000 },
+            { total: '700 Valorant Points', jumlah: 700, harga: 75000 },
+            { total: '1475 Valorant Points', jumlah: 1475, harga: 170000 },
+            { total: '2050 Valorant Points', jumlah: 2050, harga: 227000 },
+            { total: '2525 Valorant Points', jumlah: 2525, harga: 284000 },
+            { total: '3050 Valorant Points', jumlah: 3050, harga: 341000 },
+            { total: '3650 Valorant Points', jumlah: 3650, harga: 394000 },
+            { total: '4100 Valorant Points', jumlah: 4100, harga: 454000 },
+        ]
+    },
+    'MAGICCHESS': {
+        id: 'MAGICCHESS',
+        nama: 'Magic Chess Pass',
+        gambar: 'gambar/ml.png',
+        diamond_image: 'gambar/3.png',
+        deskripsi: 'Beli Magic Chess Pass untuk mendapatkan hero, skin, dan bonus eksklusif lainnya!',
+        diamond: [
+            { total: 'WDP (1x)', jumlah: 1, harga: 29000 },
+            { total: 'WDP (2x)', jumlah: 1, harga: 62000 },
+            { total: 'WDP (2x)', jumlah: 1, harga: 89000 },
+            { total: '59 Diamons (53 + 6 Bonus)', jumlah: 1, harga: 16000 },
+            { total: '170 Diamons (154 + 16 Bonus)', jumlah: 1, harga: 47000 },
+            { total: '240 Diamons (217 + 23 Bonus)', jumlah: 1, harga: 67000 },
+            { total: '875 Diamons (774 + 101 Bonus)', jumlah: 1, harga: 230000 }
+        ]
+    },
+    'WDP': {
+        id: 'WDP',
+        nama: 'Weekly Diamond Pass',
+        gambar: 'gambar/wdp.png',
+        diamond_image: 'gambar/2.png',
+        deskripsi: 'Dapatkan Diamond setiap hari selama 7 hari dengan Weekly Diamond Pass. Nilai terbaik untuk para pemain setia!',
+        diamond: [
+            { total: 'Weekly Diamond Pass (1x)', jumlah: 70, harga: 29000 },
+            { total: 'Weekly Diamond Pass (2x)', jumlah: 70, harga: 57000 },
+            { total: 'Weekly Diamond Pass (3x)', jumlah: 70, harga: 85000 },
+            { total: 'Weekly Diamond Pass (5x)', jumlah: 280, harga: 142000 }
+        ]
+    }
+};
